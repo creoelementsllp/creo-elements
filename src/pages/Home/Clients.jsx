8b6fab73b15a43f5902da311c889902e1a881e09
creoelementsllp <creoelementsdm@gmail.com>
2024-10-25 15:23:06 +0530

    building

--- src/pages/Home/Clients.jsx
+++ src/pages/Home/Clients.jsx
@@ -13,41 +13,41 @@ import { FreeMode, Pagination, Autoplay } from 'swiper/modules';
 
 export const logos1 = [
   'https://backend.creo-elements.com/wp-content/uploads/2024/07/ATULKASBEKAR.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/02/Preeti-McConkey.jpeg',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/02/19022024-Revised-logo-with-straight-India-Vietnam-Star.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/10/svgviewer-output-27.svg',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/05/peas-logo-2.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/9.jpg', //Sila
-  'http://backend.creo-elements.com/wp-content/uploads/2024/10/Smasher-Logo-NoBg.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/04/United-Logo-on-CREO.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/11.jpg', //Littlethingscute
-  'http://backend.creo-elements.com/wp-content/uploads/2024/04/CSP-logo.webp',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/09/WhatsApp-Image-2023-09-06-at-18.03.51.jpeg', //ether
-  'http://backend.creo-elements.com/wp-content/uploads/2024/02/Zircon-for-CREO-website.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/10.jpg', //innershift
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/5.jpg', //project co.
-  'http://backend.creo-elements.com/wp-content/uploads/2024/01/kidsandbagstore-new-logo.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/4.jpg', //doodly
+  'https://backend.creo-elements.com/wp-content/uploads/2024/02/Preeti-McConkey.jpeg',
+  'https://backend.creo-elements.com/wp-content/uploads/2024/02/19022024-Revised-logo-with-straight-India-Vietnam-Star.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2024/10/svgviewer-output-27.svg',
+  'https://backend.creo-elements.com/wp-content/uploads/2024/05/peas-logo-2.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/9.jpg', //Sila
+  'https://backend.creo-elements.com/wp-content/uploads/2024/10/Smasher-Logo-NoBg.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2024/04/United-Logo-on-CREO.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/11.jpg', //Littlethingscute
+  'https://backend.creo-elements.com/wp-content/uploads/2024/04/CSP-logo.webp',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/09/WhatsApp-Image-2023-09-06-at-18.03.51.jpeg', //ether
+  'https://backend.creo-elements.com/wp-content/uploads/2024/02/Zircon-for-CREO-website.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/10.jpg', //innershift
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/5.jpg', //project co.
+  'https://backend.creo-elements.com/wp-content/uploads/2024/01/kidsandbagstore-new-logo.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/4.jpg', //doodly
 ];
 
 // Second array of image URLs (for the second swiper)
 export const logos2 = [
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/2.jpg', //hsbc
-  'http://backend.creo-elements.com/wp-content/uploads/2024/02/svgviewer-png-output-2.png', //trupsel
-  'http://backend.creo-elements.com/wp-content/uploads/2024/04/MM-Bharwada-LOGO.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/10/ALLESTATE.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/2.jpg', //hsbc
+  'https://backend.creo-elements.com/wp-content/uploads/2024/02/svgviewer-png-output-2.png', //trupsel
+  'https://backend.creo-elements.com/wp-content/uploads/2024/04/MM-Bharwada-LOGO.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/10/ALLESTATE.png',
   'https:/backend.creo-elements.com/wp-content/uploads/2024/10/Ray-Logo.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/08/sleepy-tots.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/08/sleepy-tots.png',
   'https:/backend.creo-elements.com/wp-content/uploads/2024/10/Symbol-Black@4x.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/09/mtbalogo-1.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/09/mtbalogo-1.png',
   'https:/backend.creo-elements.com/wp-content/uploads/2024/08/parinie-icon.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2024/06/natasha-scaled.jpg',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/08/social-toast.png',
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/8.jpg', //artangle90
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/7.jpg', //coolab project
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/3.jpg', // Be desi
-  'http://backend.creo-elements.com/wp-content/uploads/2023/03/6.jpg', //FNQ
-  'http://backend.creo-elements.com/wp-content/uploads/2023/10/CREATIVE-WINDOW.png', //360 south
+  'https://backend.creo-elements.com/wp-content/uploads/2024/06/natasha-scaled.jpg',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/08/social-toast.png',
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/8.jpg', //artangle90
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/7.jpg', //coolab project
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/3.jpg', // Be desi
+  'https://backend.creo-elements.com/wp-content/uploads/2023/03/6.jpg', //FNQ
+  'https://backend.creo-elements.com/wp-content/uploads/2023/10/CREATIVE-WINDOW.png', //360 south
 ];
 
 
