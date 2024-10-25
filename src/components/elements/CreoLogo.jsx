02e99dfbe067281ca50ab3a6482590f2d81689e0
creoelementsllp <creoelementsdm@gmail.com>
2024-10-25 15:20:29 +0530

    building

--- src/components/elements/CreoLogo.jsx
+++ src/components/elements/CreoLogo.jsx
@@ -4,6 +4,6 @@ import { Link } from 'react-router-dom';
 export const CreoLogo = () => {
   return (
     <Link to="/">
-    <img src='/images/logo.png' className='creo-logo-top z-2 clickable'></img></Link>
+    <img src='/images/logo.png' className='creo-logo-top z-2 clickable' alt="Creo Elements LLP Logo"></img></Link>
   )
 }
